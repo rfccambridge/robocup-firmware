efad84f219042b4d20ce282a0ea248226401fea1
WFU <wfu@college.harvard.edu>
2017-12-20 12:44:39 -0800

    Added arduino xbee lib

--- testxbee/testxbee.ino
+++ testxbee/testxbee.ino
@@ -1,48 +1,43 @@
-        /*
-          Xbee1
-          D. Thiebaut
-         
-          Makes Arduino send 1 character via XBee wireless to another XBee connected
-          to a computer via a USB cable.
 
-          The circuit:
-          * RX is digital pin 2 (connect to TX of XBee)
-          * TX is digital pin 3 (connect to RX of XBee)
-         
-          Based on a sketch created back in the mists of time by Tom Igoe
-          itself based on Mikal Hart's example
-         
-        */
+#include <SoftwareSerial.h>
+#include <XBee.h>
 
-        #include <SoftwareSerial.h>
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();
+Rx64Response rx64 = Rx64Response();
 
-        SoftwareSerial xbee(2, 3); // RX, TX
-        char c = 'A';
-        int  pingPong = 1;
+int dataLED = 13;
 
-        void setup()  {
-           Serial.begin(57600);
-           Serial.println( "Arduino started sending bytes via XBee" );
+uint8_t data = 0;
+uint8_t option = 0;
 
-           // set the data rate for the SoftwareSerial port
-           xbee.begin( 19200 );
-        }
+void setup()  {
+  Serial.begin(57600);
+  xbee.setSerial(Serial);
+}
 
-        void loop()  {
+void loop()  {
+  xbee.readPacket();
+  if (xbee.getResponse().isAvailable()) {
+    digitalWrite(dataLED, HIGH);
 
-        if (Serial.available()) {
-          digitalWrite(13, HIGH);
-          Serial.println(xbee.read());
-        } else {
-          digitalWrite(13, LOW);
-          Serial.println(".");
-        }
-          //--- switch LED on Arduino board every character sent---
-          //if ( pingPong == 0 )
-          //  digitalWrite(13, LOW);
-          //else
-          //  digitalWrite(13, HIGH);
-          delay(1000);
-        }
+    if (xbee.getResponse().getApiId() == RX_16_RESPONSE ||
+        xbee.getResponse().getApiId() == RX_64_RESPONSE) {
+      // get a rx packet
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+        xbee.getResponse().getRx16Response(rx16);
+        data = rx16.getData(0);
+        Serial.println(data);
+      }
+      if (xbee.getResponse().getApiId() == RX_64_RESPONSE) {
+        xbee.getResponse().getRx64Response(rx64);
+        data = rx64.getData(0);
+        Serial.print(data);
+      }
+    }
+  }
+  Serial.println(".");
+}
 
 
