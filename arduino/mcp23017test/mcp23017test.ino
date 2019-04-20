7649b49b864aa143e2f13a353bf8a68105f0acef
John Keszler <keszler@college.harvard.edu>
2019-04-19 23:09:13 -0400

    motor testing

--- arduino/mcp23017test/mcp23017test.ino
+++ arduino/mcp23017test/mcp23017test.ino
@@ -1,4 +1,8 @@
-#define SPEED_1 10
+#define SPEED_1 2
+#define SPEED_2 3
+#define SPEED_3 10
+#define SPEED_4 29
+
 
 #define LED 13
 /*
@@ -11,6 +15,10 @@
 void setup()
 {
    pinMode(SPEED_1, OUTPUT);
+   pinMode(SPEED_2, OUTPUT);
+   pinMode(SPEED_3, OUTPUT);
+   pinMode(SPEED_4, OUTPUT);
+  
   pinMode(LED,OUTPUT);
  Wire.begin(); // wake up I2C bus
 // set I/O pins to outputs
@@ -19,6 +27,14 @@ void setup()
  Wire.write(0x00); // set all of port A to outputs
  Wire.endTransmission();
 Wire.beginTransmission(0x20);
+ Wire.write(0x01); // IODIRB register
+ Wire.write(0x00); // set all of port B to outputs
+ Wire.endTransmission();
+ Wire.beginTransmission(0x24);
+ Wire.write(0x00); // IODIRA register
+ Wire.write(0x00); // set all of port A to outputs
+ Wire.endTransmission();
+Wire.beginTransmission(0x24);
  Wire.write(0x01); // IODIRB register
  Wire.write(0x00); // set all of port B to outputs
  Wire.endTransmission();
@@ -41,16 +57,48 @@ void loop()
 {
 
 Wire.beginTransmission(0x20);
-Wire.write(0x13); // address port B 
-Wire.write(0x18);     // value to send
+Wire.write(0x13); // address port B  //top left motor (upside down one)
+Wire.write(0x0E);     // value to send
 Wire.endTransmission();
+
+Wire.beginTransmission(0x20);
+ Wire.write(0x12); // GPIOB
+ Wire.write(0x70); // port B
+ Wire.endTransmission();
+
+ Wire.beginTransmission(0x24); //right side
+Wire.write(0x13); // address port B  //top left motor (upside down one)
+Wire.write(0x0E);     // value to send
+Wire.endTransmission();
+
+Wire.beginTransmission(0x24);
+ Wire.write(0x12); // GPIOB
+ Wire.write(0x70); // port B
+ Wire.endTransmission();
+ 
+ 
  //binaryCount();
    digitalWrite(LED, HIGH); 
-   analogWrite(SPEED_1, 40);
+   analogWrite(SPEED_1, 255);
+   analogWrite(SPEED_2, 255);
+   analogWrite(SPEED_3, 255);
+   analogWrite(SPEED_4, 255);
    delay(2000);
    Wire.beginTransmission(0x20);
     Wire.write(0x13); // address port B 
-    Wire.write(0x14);     // value to send
+    Wire.write(0x00);     // value to send
     Wire.endTransmission();
+    Wire.beginTransmission(0x20);
+ Wire.write(0x12); // GPIOB
+ Wire.write(0x00); // port B
+ Wire.endTransmission();
+     Wire.beginTransmission(0x24);
+ Wire.write(0x13); // GPIOB
+ Wire.write(0x00); // port B
+ Wire.endTransmission();
+     Wire.beginTransmission(0x24);
+ Wire.write(0x12); // GPIOB
+ Wire.write(0x00); // port B
+ Wire.endTransmission();
    digitalWrite(LED, LOW);
    delay(2000);}
