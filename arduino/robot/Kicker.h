275930ec6a12bde2c5f92f548667ce94c10e8f46
grossmanlev <lgrossman@college.harvard.edu>
2019-05-09 11:06:04 -0400

    [kicker] comment kicker functions

--- arduino/robot/Kicker.h
+++ arduino/robot/Kicker.h
@@ -7,9 +7,19 @@
 class Kicker {
 public:
     Kicker();
-    void kick();
+    // wati and kick until we get to a certain charge - don't kick unless 
+    // we are at the charge level specified +- 5%
+    void kick(double charge_level);
+    // force kick - try kicking no matter how charged the capacitor is.
+    void force_kick();
+    // Tries to discharge the capacitors without kicking for safety reasons
     void discharge();
+    // Keeps the capacitor charged to a certain percentage - makes it faster
+    // to kick at the right speed.
+    void charge(double charge_level);
 private:
+    // Percentage of capacitor charge - should be proportional to kick power.
+    // double between 0 and 1.
     double charge;
 };
 
